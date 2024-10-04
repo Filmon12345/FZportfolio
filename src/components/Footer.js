f9496a798e45cca21e594349675079ab3210e9c7
Filmona <zimilifila3676@gmail.com>
2024-10-04 16:58:22 +0300

    Background picture updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Philimona Zinabu</h3>
+          <h3>Designed and Developed by Filmon Zinabu</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} FZ</h3>
