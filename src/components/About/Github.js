b763528056c0460e3b8d0bb8e840dc7b765008ca
Filmona <zimilifila3676@gmail.com>
2024-10-04 16:27:23 +0300

    github bar color changed

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,13 +6,13 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        My <strong className="purple">Codeing</strong> Days
+        My <strong className="purple">Coding</strong> Days
       </h1>
       <GitHubCalendar
         username="Filmon12345"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#e0e41d"
         fontSize={16}
       />
     </Row>
